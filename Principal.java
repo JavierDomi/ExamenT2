cae207c99c713ee394e96ac15f5bec1071d72e07
JavierDomi <113978972+JavierDomi@users.noreply.github.com>
2023-03-17 11:32:59 +0100

    GitHub1

--- Principal.java
+++ Principal.java
@@ -1,8 +1,5 @@
 	public static void main(String[] args) {
 	System.out.Println("Hola a todas");
-<<<<<<< HEAD
 	System.out.Println("Punto j");
-=======
-	
->>>>>>> RC
+	System.out.Println("GitHub");
 	}
